09ef0a056a50b5d0241cebd24d522c0e9039c788
alexander <sosenko1995@gmail.com>
2019-12-09 22:07:36 +0300

    Play with file utilities.

--- lesson5/main.pl
+++ lesson5/main.pl
@@ -1,8 +1,12 @@
 use strict;
 use warnings;
+use File::Basename;
+use File::Spec::Functions;
 
 sub main {
-    print 'Hello, World!';
+    my $basedir = dirname(__FILE__);
+    my $file = catfile($basedir, 'mymanjeeves.txt');
+    print $file;
 }
 
 main();
