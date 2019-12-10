4577f7f0f2821fb7085ab512ec40b64647738e38
sosenko1995 <sosenko1995@gmail.com>
2019-12-10 07:41:59 +0300

    Use perlcritic to check the code.

--- lesson5/main.pl
+++ lesson5/main.pl
@@ -1,20 +1,21 @@
 use strict;
 use warnings;
+use English;
 use File::Basename;
 use File::Spec::Functions;
 
 sub main {
     my $basedir = dirname(__FILE__);
     my $file = catfile($basedir, 'mymanjeeves.txt');
-    open(INPUT, $file) or die("Input file $file not found.\n");
+    open(my $INPUT, '<', $file) or die("Can't open $file: $OS_ERROR.\n");
 
-    while (my $line = <INPUT>) {
+    while (my $line = <$INPUT>) {
         if ($line =~ / egg /) {
             print $line;
         }
     }
 
-    close(INPUT);
+    close($INPUT);
 }
 
 main();
