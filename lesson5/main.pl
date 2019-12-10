f8f1df0158ebc03afb8d32e0448262753d320366
sosenko1995 <sosenko1995@gmail.com>
2019-12-10 07:28:42 +0300

    Use simple regular expression.

--- lesson5/main.pl
+++ lesson5/main.pl
@@ -6,7 +6,15 @@ use File::Spec::Functions;
 sub main {
     my $basedir = dirname(__FILE__);
     my $file = catfile($basedir, 'mymanjeeves.txt');
-    print $file;
+    open(INPUT, $file) or die("Input file $file not found.\n");
+
+    while (my $line = <INPUT>) {
+        if ($line =~ / egg /) {
+            print $line;
+        }
+    }
+
+    close(INPUT);
 }
 
 main();
